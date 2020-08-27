17d4a1de912eeb2307f88f68a0fb8349deed1e5b
Tom Baker <tom@tombaker.org>
2020-08-27 13:14:16 +0200

    update documentation

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
-# This command creates (or refreshes) the website at https://dcmi.github.io/ldci/
-# The command must be run from the root directory of the https://github.com/dcmi/ldci/ repo
+# This command creates (or refreshes) the website at https://dcmi.github.io/dcmi_events/
+# See https://www.mkdocs.org/user-guide/deploying-your-docs/
+# This command must be run from the root directory of the https://github.com/dcmi/dcmi_events/ repo
 # MkDocs must be installed - see http://www.mkdocs.org/#installation
 # Behind the scenes, MkDocs will build your docs and use the ghp-import tool to commit them 
 # to the gh-pages branch and push the gh-pages branch to GitHub.
